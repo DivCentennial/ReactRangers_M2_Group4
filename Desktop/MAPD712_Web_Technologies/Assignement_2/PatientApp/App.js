8dcd68974b21ad9d5f23e52bd6523fdfcec3a4d8
kashishksy <kashishksy2112@gmail.com>
2024-10-22 08:07:56 -0400

    pushing App.js changes for 2 new screens

--- Desktop/MAPD712_Web_Technologies/Assignement_2/PatientApp/App.js
+++ Desktop/MAPD712_Web_Technologies/Assignement_2/PatientApp/App.js
@@ -6,6 +6,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import DashboardScreen from './screens/DashboardScreen';
 import SignUpScreen from './screens/SignUpScreen';
+import AddPatientScreen from './screens/AddPatientScreen';
+import PatientDetailsScreen from './screens/PatientDetailsScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,6 +18,8 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="AddPatient" component={AddPatientScreen} />
+        <Stack.Screen name="PatientDetails" component={PatientDetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
