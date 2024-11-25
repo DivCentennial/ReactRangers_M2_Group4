1580b5d521b89c6479bb27c89a0a4a5b1ef2f1d8
kashishksy <kashishksy2112@gmail.com>
2024-11-24 20:51:46 -0500

    react native gesture handler mock file

---
+++ Desktop/MAPD712_Web_Technologies/Assignement_2/PatientApp/__mocks__/react-native-gesture-handler.js
@@ -0,0 +1,15 @@
+// __mocks__/react-native-gesture-handler.js
+import { View } from 'react-native';
+
+export const State = {};
+export const PanGestureHandler = View;
+export const TapGestureHandler = View;
+export const Swipeable = View;
+export const GestureHandlerRootView = View;
+export const RectButton = View;
+export const NativeViewGestureHandler = View;
+export const TouchableOpacity = View;
+export const TouchableWithoutFeedback = View;
+export const TouchableHighlight = View;
+
+export default {};
